f6cf679313e58cc3fbd7735d6cf4c9ca5448c7ef
NeoVertex1 <toymathematician@proton.me>
2024-08-08 12:34:20 -0400

    Update mis_tests.py

--- mis_tests.py
+++ mis_tests.py
@@ -1,4 +1,4 @@
-# these a comprehensive tests, these are "proof" of all mis known properties, have in mind the invariance breaks down with time very quickly we need to learn about it further
+# here are some comprehensive tests, these are "proof" of all mis known properties, have in mind the invariance breaks down with time very quickly we need to learn about it further
 
 import mpmath as mp
 from scipy import stats
